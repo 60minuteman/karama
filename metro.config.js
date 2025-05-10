24149ce5a2f1862d49ba3abca35b13d742bb2777
modibest03 <modibest03@gmail.com>
2025-05-10 12:19:49 +0100

    working

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,20 @@
+// metro.config.js
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
-
-module.exports = config; 
+  return {
+    ...config,
+    transformer: {
+      ...config.transformer,
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'], // Ensures asset handling
+    },
+    resolver: {
+      ...config.resolver,
+      assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...config.resolver.sourceExts, 'svg'],
+    },
+  };
+})();
